73c7703ebb4bcb17b0c269b35c0a8426c08690c3
Oswaldo Alvarez <oswal3000@hotmail.com>
2016-11-15 17:57:03 -0500

    building landing

--- thewall9.v2.web/gulpfile.js
+++ thewall9.v2.web/gulpfile.js
@@ -9,6 +9,8 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var svgstore = require('gulp-svgstore');
+var path = require('path');
 
 /**
  * CSS
@@ -49,11 +51,28 @@ gulp.task('postcss', ['less'], function () {
  * JS
  */
 gulp.task('minify:vendors', function () {
-    return  gulp.src([
+    return gulp.src([
         './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.min.js'
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/granim/dist/granim.min.js',
     ])
         .pipe(concat('vendors.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+});
+gulp.task('minify', ['minify:home'], function () {
+    return gulp.src([
+        './src/js/app.js',
+    ])
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+});
+gulp.task('minify:home', function () {
+    return gulp.src([
+        './src/js/app.home.js',
+    ])
+        .pipe(concat('app.home.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('build/js/'))
 });
@@ -63,7 +82,16 @@ gulp.task('minify:vendors', function () {
 gulp.task('clean', (cb) => {
     return del.sync(["build"], cb);
 });
-
+/**
+ * SVG
+ */
+gulp.task('svg', function () {
+    //return gulp
+    //    .src('src/svg/**/*.svg')
+    //    .pipe(svgstore())
+    //    .pipe(rename('icons.svg'))
+    //    .pipe(gulp.dest('build/svg/'));
+});
 /**
  * Copy all resources that are not TypeScript files into build directory.
  */
@@ -84,14 +112,17 @@ gulp.task('watch', function () {
     gulp.watch(["./src/css/**/*.less"], ['cssmin']).on('change', function (e) {
         console.log('Resource file ' + e.path + ' has been changed. Updating.');
     });
+    gulp.watch(["./src/js/**/*.js"], ['minify']).on('change', function (e) {
+        console.log('Resource file ' + e.path + ' has been changed. Updating.');
+    });
 });
 
 /**
  * Build the project.
  */
-gulp.task("build", ['clean', 'resources', 'minify:vendors', 'cssmin:vendors', 'cssmin'], () => {
+gulp.task("build", ['clean', 'resources', 'minify:vendors', 'minify', 'cssmin:vendors', 'cssmin', 'svg'], () => {
     console.log("Building the project ...");
 });
-gulp.task("build-watch", ['clean','resources', 'minify:vendors', 'cssmin:vendors', 'cssmin', 'watch'], () => {
+gulp.task("build-watch", ['clean', 'resources', 'minify:vendors', 'minify', 'cssmin:vendors', 'cssmin','svg', 'watch'], () => {
     console.log("Building the project ...");
 });
--- thewall9.v2.web/src/js/app-home.js
+++
@@ -1,5 +0,0 @@
-﻿$(function () {
-
-});
-$(document).ready(function () {
-});
