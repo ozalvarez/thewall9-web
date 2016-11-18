7c78ddda635c62f9055e18e22f9be9aeb8c70597
Oswaldo Alvarez <oswal3000@hotmail.com>
2016-11-17 20:08:59 -0500

    update references

--- thewall9.v2.web/gulpfile.js
+++ thewall9.v2.web/gulpfile.js
@@ -59,6 +59,7 @@ gulp.task('minify:vendors', function () {
         './node_modules/toastr/build/toastr.min.js',
         './node_modules/reframe.js/dist/reframe.js',
         './node_modules/progressbar.js/dist/progressbar.min.js',
+        './src/js/others/*.js',
     ])
         .pipe(concat('vendors.min.js'))
         .pipe(uglify())
